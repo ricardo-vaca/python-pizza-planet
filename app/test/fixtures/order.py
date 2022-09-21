23f952bbce879fdab86c6836ec11fc339bf20bda
Ricardo Vaca <ricardo.vaca@ioet.com>
2022-09-20 21:48:22 -0500

    PIZ2.1: Add tests for the beverage model
    TDD: Adding beverage model tests and modifying order model tests

--- app/test/fixtures/order.py
+++ app/test/fixtures/order.py
@@ -24,12 +24,14 @@ def client_data():
 
 
 @pytest.fixture
-def order(create_ingredients, create_size, client_data) -> dict:
+def order(create_ingredients, create_beverages, create_size, client_data) -> dict:
     ingredients = [ingredient.get('_id') for ingredient in create_ingredients]
+    beverages = [beverage.get('_id') for beverage in create_beverages]
     size_id = create_size.get('_id')
     return {
         **client_data_mock(),
         'ingredients': ingredients,
+        'beverages': beverages,
         'size_id': size_id
     }
 
