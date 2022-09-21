07b826a72877bb92daa08e11a1b7fa6cafbb7ff0
Ricardo Vaca <ricardo.vaca@ioet.com>
2022-09-20 21:51:37 -0500

    PIZ2.2: Remove null values for 'get_order_by_id' endpoint
    Adding 'remove_null_order_details' function to utils

--- app/common/utils.py
+++ app/common/utils.py
@@ -1,3 +1,14 @@
 
 def check_required_keys(keys: tuple, element: dict):
     return all(element.get(key) for key in keys)
+
+
+def remove_null_order_details(order):
+    new_list = []
+    for detail in order['detail']:
+        new_dict = {
+            key: value for key, value in detail.items() if value is not None
+        }
+        new_list.append(new_dict)
+    order.update({'detail': new_list})
+    return order
