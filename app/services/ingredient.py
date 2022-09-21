eec2fd8030a7986e3d278eedb97e3fbd157bad52
Ricardo Vaca <ricardo.vaca@ioet.com>
2022-09-21 14:42:45 -0500

    Refactor 1: API Services
    Creating a 'base_service' function that receives required params

--- app/services/ingredient.py
+++ app/services/ingredient.py
@@ -1,38 +1,43 @@
-from app.common.http_methods import GET, POST, PUT
 from flask import Blueprint, jsonify, request
 
+from app.common.http_methods import GET, POST, PUT
+
+from .base import base_service
 from ..controllers import IngredientController
 
 ingredient = Blueprint('ingredient', __name__)
 
 
-@ingredient.route('/', methods=POST)
-def create_ingredient():
-    ingredient, error = IngredientController.create(request.json)
-    response = ingredient if not error else {'error': error}
-    status_code = 200 if not error else 400
-    return jsonify(response), status_code
-
-
-@ingredient.route('/', methods=PUT)
-def update_ingredient():
-    ingredient, error = IngredientController.update(request.json)
-    response = ingredient if not error else {'error': error}
-    status_code = 200 if not error else 400
-    return jsonify(response), status_code
+@ingredient.route('/', methods=GET)
+def get_ingredients():
+    return base_service(
+        IngredientController,
+        method=GET,
+    )
 
 
 @ingredient.route('/id/<_id>', methods=GET)
 def get_ingredient_by_id(_id: int):
-    ingredient, error = IngredientController.get_by_id(_id)
-    response = ingredient if not error else {'error': error}
-    status_code = 200 if ingredient else 404 if not error else 400
-    return jsonify(response), status_code
+    return base_service(
+        IngredientController,
+        method=GET,
+        id=_id,
+    )
 
 
-@ingredient.route('/', methods=GET)
-def get_ingredients():
-    ingredients, error = IngredientController.get_all()
-    response = ingredients if not error else {'error': error}
-    status_code = 200 if ingredients else 404 if not error else 400
-    return jsonify(response), status_code
+@ingredient.route('/', methods=POST)
+def create_ingredient():
+    return base_service(
+        IngredientController,
+        method=POST,
+        request=request.json
+    )
+
+
+@ingredient.route('/', methods=PUT)
+def update_ingredient():
+    return base_service(
+        IngredientController,
+        method=PUT,
+        request=request.json
+    )
--- app/services/size.py
+++ app/services/size.py
@@ -1,6 +1,8 @@
-from app.common.http_methods import GET, POST, PUT
 from flask import Blueprint, jsonify, request
 
+from app.common.http_methods import GET, POST, PUT
+
+from .base import base_service
 from ..controllers import SizeController
 
 size = Blueprint('size', __name__)
@@ -16,23 +18,26 @@ def get_all():
 
 @size.route('/', methods=POST)
 def create_size():
-    size, error = SizeController.create(request.json)
-    response = size if not error else {'error': error}
-    status_code = 200 if not error else 400
-    return jsonify(response), status_code
+    return base_service(
+        SizeController,
+        method=POST,
+        request=request.json
+    )
 
 
 @size.route('/', methods=PUT)
 def update_size():
-    size, error = SizeController.update(request.json)
-    response = size if not error else {'error': error}
-    status_code = 200 if not error else 400
-    return jsonify(response), status_code
+    return base_service(
+        SizeController,
+        method=PUT,
+        request=request.json
+    )
 
 
 @size.route('/id/<_id>', methods=GET)
 def get_size_by_id(_id: int):
-    size, error = SizeController.get_by_id(_id)
-    response = size if not error else {'error': error}
-    status_code = 200 if size else 404 if not error else 400
-    return jsonify(response), status_code
+    return base_service(
+        SizeController,
+        method=GET,
+        id=_id
+    )
