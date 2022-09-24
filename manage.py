21754720488a80e3fceeeb3e3d3bd878bbfbadc6
Ricardo Vaca <ricardo.vaca@ioet.com>
2022-09-23 19:29:28 -0500

    Add 'manage.py clear-db' command

--- manage.py
+++ manage.py
@@ -3,6 +3,7 @@
 import pytest
 from flask.cli import FlaskGroup
 from flask_migrate import Migrate
+from flask_seeder import FlaskSeeder
 
 from app import flask_app
 from app.plugins import db
@@ -15,10 +16,22 @@ manager = FlaskGroup(flask_app)
 migrate = Migrate()
 migrate.init_app(flask_app, db)
 
+seeder = FlaskSeeder()
+seeder.init_app(flask_app, db)
+
 
 @manager.command('test', with_appcontext=False)
 def test():
     return pytest.main(['-v', './app/test'])
+
+
+@manager.command('clear-db', with_appcontext=True)
+def clear_db():
+    meta = db.metadata
+    for table in reversed(meta.sorted_tables):
+        print('Clear table %s' % table)
+        db.session.execute(table.delete())
+    db.session.commit()
 
 
 if __name__ == '__main__':
